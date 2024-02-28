cd1d24301eee336b5e19b92d62cddbb05b4dc9a6
Conor Grace <20093293@mail.wit.ie>
2024-02-28 12:09:12 +0000

    Fixed the api version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ie.setu.borrowalift"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ie.setu.borrowalift"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
